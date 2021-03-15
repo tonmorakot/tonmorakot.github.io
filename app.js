a18c43a508e328e96efa3a555f5ef9ca97148e8d
AIR Morakot Thongprom <tonmorakot@gmail.com>
2021-03-15 15:24:30 +0700

    fix6

--- app.js
+++ app.js
@@ -1,27 +1,63 @@
+function dynamicSort(property) {
+  let sortOrder = 1;
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function(a, b) {
+    /* next line works with strings and numbers,
+     * and you may want to customize it to your needs
+     */
+    let result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+    return result * sortOrder;
+  };
+}
+
 let x = "morakot";
-let y = [
-  [11, 22, 33],
-  [33, 44, 55],
-];
+let y = [[11, 22, 33], [33, 44, 55]];
 let z = [
   {
+    time: "19/03/21",
     name: "Morakot",
     lastname: "Thongprom",
     age: 3,
   },
   {
+    time: "11/04/2020",
+    name: "Anothai",
+    lastname: "dddddddd",
+    age: 30,
+  },
+  {
+    time: "11/02/2021",
+    name: "Anothai",
+    lastname: "dddddddd",
+    age: 30,
+  },
+  {
+    time: "11/03/21",
     name: "Anothai",
     lastname: "dddddddd",
     age: 30,
   },
 ];
 
-for (const i in z) {
-  console.log(z[i].name + " " + z[i].lastname)
+let DD = z[3].time.substring(0, 2);
+let MM = z[3].time.substring(5, 3);
+let YY = parseInt(z[3].time.substring(z[3].time.length - 2, z[3].time.length));
+
+console.log(DD);
+console.log(MM);
+console.log(YY);
+
+//console.log(z.sort(dynamicSort("-time")));
+//for (const i in z) {
+//console.log(z[i].name + " " + z[i].lastname)
 //   for (const j in z[i]) {
 //     console.log();
 //   }
-}
+//}
 
 // for (const i in y) {
 //     for (const j in y[i]) {
